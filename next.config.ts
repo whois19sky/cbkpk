df50b7180ff8701fcdd09c5fdc82e60b4f90c338
whois19sky <someone.akash@hotmail.com>
2026-07-25 17:22:49 +0530

    cb extranet fixed

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'wwpiftimeoaoizerilhz.supabase.co',
+        hostname: 'mwlmrpgcapaydccxqhsu.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
       {
